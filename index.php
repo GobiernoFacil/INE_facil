8c3910f9ff3e482b3cb3b14b5225c85cce525e79
hugovom <hugo.om@gmail.com>
2015-05-28 17:20:28 -0500

    update home with new links

--- index.php
+++ index.php
@@ -25,11 +25,67 @@
 <section class="se_vota">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
-				<h2>¿Qué se elige en las Elecciones Federales 2015?</h2>
-				<p>Se eligen los 500 diputados federales que integrarán la LXIII Legislatura del Congreso de la Unión</p>
-				<p>300 diputados serán electos por mayoría simple en cada uno de los distritos electorales en que se divide el país y los 200 	
-					restantes mediante el principio de representación proporcional.</p>
+			<div class="col-md-8 col-md-offset-2">
+				<h2>¿Qué se elige en las <strong>Elecciones Federales 2015</strong>?</h2>
+				<p class="lead"><strong>500 diputados federales</strong> que integrarán la <strong>LXIII Legislatura</strong> del Congreso de la Unión.</p>
+				<div class="row">
+					<div class="col-md-6">
+						<p><i class="electos"></i> <strong>300</strong> electos por mayoría simple </p>
+						<?php echo str_repeat('<i class="electos"></i>',300);?>
+						
+					</div>
+					<div class="col-md-6">
+						<p><i class="representacion"></i>  <strong>200</strong> por representación proporcional</p>
+						<?php echo str_repeat('<i class="representacion"></i>',200);?>
+					</div>
+				</div>
+				<div class="col-md-8 col-md-offset-2">
+				<div class="links">
+						<a class="hvr-bounce-to-right" href="/diputados-federales.php">¿Cuántos Diputados federales se eligen estado por? 
+							¿Cuáles son las circunscripciones electorales 
+							de México? <strong>&gt;</strong>
+						</a>
+				</div>
+			</div>
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="se_gasta">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<h2>¿Cuánto dinero dimos los mexicanos a los partidos políticos en 2015?
+				</h2>
+				<p class="lead"><strong>$5,199 millones de pesos</strong></p>
+				
+				<div class="row">
+					<p class="labels"><span><i class="perma"></i>Actividades Ordinarias Permanentes</span>
+						<span><i class="gastos"></i>Gastos de Campaña</span>
+						<span><i class="espec"> </i>Actividades Específicas</span>
+					</p>
+					<div class="col-md-3">
+						<p class="lana">$3,909,545,803</p>
+						<p class="lana">$1,172,863,740</p>
+						<p class="lana">$117,286,374</p>
+					</div>
+					<div class="col-md-9">
+						<div class="permanentes"></div>
+						<div class="campaign"></div>
+						<div class="especificas"></div>
+					</div>
+				</div>
+								
+				<div class="col-md-8 col-md-offset-2">
+				<div class="links">
+						<a class="hvr-bounce-to-right" href="http://gobiernofacil.com/herramientas/financiamiento-publico-partidos-politicos/">Financiamiento público de los partidos políticos en México
+1997 - 2015<strong>&gt;</strong>
+						</a>
+				</div>
+			</div>
+
 			</div>
 		</div>
 	</div>
