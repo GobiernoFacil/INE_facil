ee4a180b8aacf043713e3507cef044b84ab34743
hugovom <hugo.om@gmail.com>
2015-06-01 16:52:32 -0500

    update modal view

--- templates/modal.php
+++ templates/modal.php
@@ -10,32 +10,43 @@
 			{{#if casa_campana}} {{casa_campana}} {{else}} Indigente / No Sabe / No contestó{{/if}}
 		</p>
 		<p class="info_gral"><span class="label">Correo:</span> 
-			{{#if correo}} {{correo}} {{else}} Sin correo / No Sabe / No contestó{{/if}}
+			{{#if correo}} <a href="mailto:{{correo}}" class="links">{{correo}}</a> {{else}} Sin correo / No Sabe / No contestó{{/if}}
 		</p>
 		<p class="info_gral"><span class="label">Enlaces:</span> 
-			{{#if social}} {{social}} {{else}} Sin enlaces / No Sabe / No contestó{{/if}}
+			{{#if social}} 
+				{{#each social}}
+					<a href="{{url}}" target="_blank" class="links">{{red}}</a>
+				{{/each}}
+			{{else}} 
+				Sin enlaces / No Sabe / No contestó
+			{{/if}}
 		</p>
 	</div>
 	<div class="col-sm-9">
-		<h4>Candidato a Diputado Federal, Distrito 12, Puebla</h4>
+		<h4>Candidato a Diputado Federal, Distrito {{district}}, {{city}}, {{state}}.</h4>
 		<h2>{{nombre}}</h2>
 		<p class="partidin {{#each partidos}} {{this}} {{/each}}"></p>
 		<p class="suplente"><span class="label">Suplente:</span> {{suplente}}</p>
 		<div class="mentiras">
 			{{#with mentiras}}
 				<h3><a>Propuesta</a></h3>
-				<p>{{templete}}</p>
+				{{#if templete}} 
+					<p>{{templete}}</p>
+				{{else}}
+					<p>Sin Propuesta / No Sabe / No contestó</p>
+				{{/if}}
 				<h3><a>Trayectoria en administraciones públicas</a></h3>
-				<p>{{trayectoria}}</p>			
+				{{#if trayectoria}} 
+					<p>{{trayectoria}}</p>
+				{{else}}
+					<p>Sin Trayectoria / No Sabe / No contestó</p>			
+				{{/if}}
 				<h3><a>Trayectoria laboral</a></h3>
-				<p>{{laborales}}</p>
-			{{else}}
-				<h3><a>Propuesta</a></h3>
-				<p>Sin Propuesta / No Sabe / No contestó</p>
-				<h3><a>Trayectoria en administraciones públicas</a></h3>
-				<p>Sin Trayectoria / No Sabe / No contestó</p>			
-				<h3><a>Trayectoria laboral</a></h3>
-				<p>Sin Experiencia / No Sabe / No contestó</p>
+				{{#if laborales}} 
+					<p>{{laborales}}</p>
+				{{else}}
+					<p>Sin Experiencia / No Sabe / No contestó</p>
+				{{/if}}
 			{{/with}}			
 		</div>
 	</div>
