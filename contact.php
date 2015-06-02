ba9619addc94da773d5310971e22f004ab8c0d0d
hugovom <hugo.om@gmail.com>
2015-06-01 21:10:24 -0500

    add contact page

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php	
+	$body_class 	= "contact";
+	$title 			= "Contacto sobre INE Fácil";
+	$description 	= "Envía un mensaje al equipo de INE Fácil.";
+	$canonical		= "http://inefacil.com/contacto/";
+	include $_SERVER['DOCUMENT_ROOT']."/templates/header.php";?>
+	
+<section class="container">
+	<div class="row">		
+		<div class="divider" data-text="Contáctanos"><h1>Contáctanos</h1></div>
+		<div class="col-sm-8 col-sm-offset-2">	
+			<p>Si tienes alguna duda acerca del desarrollo de <strong>INE fácil</strong>, los datos o te gustaría saber más
+				sobre transparencia, contacta al equipo de Gobierno Fácil sobre este sitio, 
+				escribe <a href="mailto:howdy@gobiernofacil.com">un mensaje</a> y te responderemos tan pronto como sea posible.</p>
+			
+			<p><strong>Antes de escribir</strong> ten en cuenta que no somos parte del <strong>INE (Instituto Nacional Electoral)</strong>, por 
+				lo que no podemos responder a comentarios sobre temas específicos del INE.</p> 
+		</div>
+	</div>
+</section>
+<?php include $_SERVER['DOCUMENT_ROOT']."/templates/footer.php";?>
