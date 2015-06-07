08b5bdce6ffb8ed42e8889c0599b5bc4132b7997
hugovom <hugo.om@gmail.com>
2015-06-07 09:34:38 -0500

    add new countdown

--- js/main.home.js
+++ js/main.home.js
@@ -2,6 +2,7 @@
 
 var today    = new Date(),
     election = new Date(2015, 5, 7, 8),
+    election_end = new Date(2015, 5, 7, 18),
     clock    = document.querySelector("#countdown"),
     timer, html, interval;
 
@@ -12,6 +13,26 @@ if(today < election){
     html = "<p>Faltan:" 
       +"<span class='number'><strong>" + timespan.days + "</strong></span> días "
       +"<span class='number'>" + timespan.hours + "</span> horas "
+      +"<span class='number'>" + timespan.minutes + "</span> minutos "
+      +"<span class='number'>" + timespan.seconds + "</span> segundos</p>";
+    clock.innerHTML = html;
+
+    if(!timespan.days && !timespan.hours && !timespan.minutes && timespan.seconds < 3){
+      clearInterval(interval);
+      clock.innerHTML = "¡A votar!!!!!";
+    }
+  }, 1000);
+}
+else{
+  clock.innerHTML = "";
+}
+
+
+if(today < election_end){
+  interval = setInterval(function(){
+    var timespan = countdown(election_end);
+    html = "<p>¡A votar! quedan:" 
+      +"<span class='number'><strong>" + timespan.hours + "</strong></span> horas "
       +"<span class='number'>" + timespan.minutes + "</span> minutos "
       +"<span class='number'>" + timespan.seconds + "</span> segundos</p>";
     clock.innerHTML = html;
